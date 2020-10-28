7424a4ef5e8671cf2ff34ae1fdcab0844e8c69fa
CamiloMaiaPires <63420640+CamiloMaiaPires@users.noreply.github.com>
2020-10-28 15:08:01 -0300

    Atualização bootstrap

---
+++ php/menu.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/estilo.css"/>
+
+    <title>Biotecnologia - UNIFESP</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+          
+          </div>
+          <div class="col titulo">
+            <h4>UNIVERSIDADE FEDERAL DE SÃO PAULO</h4>
+            <h2>BIOTECNOLOGIA</h2>
+          </div>
+          <div class="col">
+          </div>
+        </div>
+      </div>    
+    </header>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#"></a>
+      <img src="../img/logobiotec_transparente.png" width="100" height="95"/>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Graduação
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">O que é Biotecnologia</a>
+              <a class="dropdown-item" href="#">Forma de Ingresso</a>
+              <a class="dropdown-item" href="#">Atuação Profissional</a>
+              <a class="dropdown-item" href="#">Projeto Pedagógico</a>
+              <a class="dropdown-item" href="#">Matriz curricular</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Docentes
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Professores da Biotecnologia </a>
+              <a class="dropdown-item" href="#">Laboratórios e linhas de pesquisa</a>
+              <a class="dropdown-item" href="#">Comissão de Curso</a>
+              <a class="dropdown-item" href="#">Professores do Campus UNIFESP- SJC</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Alunos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Ex- Alunos</a>
+              <a class="dropdown-item" href="#">Trabalho de conclusão de Curso</a>
+              <a class="dropdown-item" href="#">Estágio não obrigatório</a>
+              <a class="dropdown-item" href="#">Monitorias e Tutorias</a>
+              <a class="dropdown-item" href="#">Biblioteca</a>
+              <a class="dropdown-item" href="#">NAE- Núcleo de Apoio ao Estudante</a>
+              <a class="dropdown-item" href="#">GTAE-Grupo Técnico de Apoio ao Ensino</a>
+              <a class="dropdown-item" href="#">DAE- Divisão de Assuntos Educacionais</a>
+              <a class="dropdown-item" href="#">Projetos Estudantis do Campus</a>
+              <a class="dropdown-item" href="#">Portal do Estudante</a>
+            </div>
+          </li>
+          <li class="nav-item">
+        <a class="nav-link" href="#">Pós Graduação</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre</a>
+      </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+        </form>
+      </div>
+    </nav>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
